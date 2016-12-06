6d337fb63cfc832cca1111d38fc41ce8194c8a28
brials <brials008@hotmail.com>
2016-12-06 14:38:22 -0800

    finished footer function render website appropriately with no css

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var shopObjects = [];
 var locationData = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 var minData = [23, 3, 11, 20, 2];
 var maxData = [65, 24, 38, 38, 16];
-var avgData = [6.3, 1.2, 3.7, 2.3, 4.6];
+var avgData = [6.3, 1.2, 3.7, 2.3, 4.6]; // for all these arrays positions match their data across.
 
 function Store(shop, min, max, avg) {
   this.shopName = shop;
@@ -20,7 +20,7 @@ function Store(shop, min, max, avg) {
     return random;
   }
 
-  this.avgSoldPH = function(){
+  this.avgSoldPH = function(){ //adds the random data to the array
     for(var i = 0; i < hours.length; i++){
       var temp = Math.floor(this.randCustPH() * this.avgCookieSalePD);
       this.cookiesSoldHourlyData.push(temp);
@@ -28,7 +28,7 @@ function Store(shop, min, max, avg) {
     }
   }
 
-  this.makeRow = function(){
+  this.makeRow = function(){  //This is my render method
     this.avgSoldPH();
     var trEl = document.createElement('tr');
     var place = document.getElementById('storeTable')
@@ -48,21 +48,53 @@ function Store(shop, min, max, avg) {
 }
 
 
-// function tableCreate(){
-//   var thEl = document.createElement('th');
-//   var place = document.getElementById('storeTable');
-//   place.appendChild(thEl);
-//   for(var i = 0; i < hours.length; i++){
-//     thEl = document.createElement('th');
-//     thEl.textContent = hours[i];
-//     place.appendChild(thEl);
-//   }
-// }
+function tableCreateHeader(){ // Header function
+  var thEl = document.createElement('th');
+  var place = document.getElementById('storeTable');
+  place.appendChild(thEl);
+  for(var i = 0; i < hours.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    place.appendChild(thEl);
+  }
+  thEl = document.createElement('th');
+  thEl.textContent = 'Total';
+  place.appendChild(thEl);
+}
 
-function arrayShops(){
+function arrayShops(){  // Stores all objects in an array and runs the makeRow function for each of them.
   for(var i = 0; i < locationData.length; i++){
     var temp = new Store(locationData[i], minData[i], maxData[i], avgData[i]);
     shopObjects.push(temp);
     temp.makeRow();
   }
 }
+
+function tableCreateFooter(){  //Makes the footer that adds up the total for each object at each hour, and totals the toal counts.
+  var trEl = document.createElement('tr');  // This function must be run after arrayShops.
+  var place = document.getElementById('storeTable')
+  place.appendChild(trEl);
+  var tdEl = document.createElement('td');
+  tdEl.textContent = 'Total';
+  trEl.appendChild(tdEl);
+  for(var i = 0; i < hours.length; i++){
+    var temp = 0;
+    for(var j = 0; j < shopObjects.length; j++){
+      temp += shopObjects[j].cookiesSoldHourlyData[i];
+    }
+    tdEl = document.createElement('td');
+    tdEl.textContent = temp;
+    trEl.appendChild(tdEl);
+  }
+  var overallTotal = 0;
+  for(var k = 0; k < shopObjects.length; k++){
+    overallTotal += shopObjects[k].totalCookiesSold;
+  }
+  tdEl = document.createElement('td');
+  tdEl.textContent = overallTotal;
+  trEl.appendChild(tdEl);
+}
+
+tableCreateHeader();
+arrayShops();
+tableCreateFooter();
